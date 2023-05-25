8cb901e4f5ffbb95011a27d2f6229a5a1326f26a
kakzaki <kakzaki@gmail.com>
2023-05-25 11:05:59 +0700

    init

--- example/force_type_example.dart
+++ example/force_type_example.dart
@@ -1,6 +1,34 @@
 import 'package:force_type/force_type.dart';
+import 'package:test/test.dart';
 
 void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+  group('A group of tests', () {
+    final dynamicA = 123;
+    final dynamicB = "123";
+    final dynamicC = "123.456";
+    final dynamicD = "true";
+    final dynamicE = "1";
+    final dynamicF = "1969-07-20 20:18:04Z";
+
+    test('Force String Test', () {
+      expect(dynamicA.forceString, "123");
+    });
+
+    test('Force Integer Test', () {
+      expect(dynamicB.forceInteger, 123);
+    });
+
+    test('Force Double Test', () {
+      expect(dynamicC.forceDouble, 123.456);
+    });
+
+    test('Force Boolean Test', () {
+      expect(dynamicD.forceBoolean, true);
+      expect(dynamicE.forceBoolean, true);
+    });
+
+    test('Force DateTime Test', () {
+      expect(dynamicF.forceDateTime, DateTime.parse("1969-07-20 20:18:04Z"));
+    });
+  });
 }
